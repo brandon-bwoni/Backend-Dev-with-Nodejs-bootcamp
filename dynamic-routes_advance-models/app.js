a5659fd19cd96a177b6af42760f96a90c20bc74e
Brandon Bwoni <bwonibrandon@gmail.com>
2023-09-02 19:33:16 +0530

    dynamic-routes & advanced-models

--- dynamic-routes_advance-models/app.js
+++ dynamic-routes_advance-models/app.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const port = 3000;
 
 const errorController = require('./controllers/error');
 
@@ -17,11 +16,9 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(3000);
